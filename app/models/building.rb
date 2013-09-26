4c64a08858fd18f21243c17bf5af71973a4fe49b
Zach Young <zachjyoung@gmail.com>
2013-09-26 17:10:29 -0400

    state code working tests passing very ugly

--- app/models/building.rb
+++ app/models/building.rb
@@ -4,4 +4,10 @@ class Building < ActiveRecord::Base
   validates_presence_of :state
   validates_presence_of :postal_code
   validates_presence_of :description
+  
+  validates_format_of :street_address, :without => /foo/
+  validates_format_of :city, :without => /foo/
+  validates_format_of :state, :with => /(?-i:A[LKSZRAEP]|C[AOT]|D[EC]|F[LM]|G[AU]|HI|I[ADLN]|K[SY]|LA|M[ADEHINOPST]|N[CDEHJMVY]|O[HKR]|P[ARW]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY])/
+  validates_format_of :postal_code, :without => /foo/
 end
+
