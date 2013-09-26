b2f105a6fc837c8ca962ee2873b12cb3a619d530
Zach Young <zachjyoung@gmail.com>
2013-09-26 13:47:58 -0400

    initial commit starting over

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewEstate::Application.config.secret_key_base = '096ca35a853f91f44a67ae4e726c1fa55198d9e069b6989081a16eb4422057c971eb106d22d18f99bf1dd4d9e217f3cfed3e4e1ad9b9f7f63de6de215c103961'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NewEstate::Application.config.session_store :cookie_store, key: '_new_estate_session'
